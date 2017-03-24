576e73b832fde5989ac6e66bc8b4412230eedf23
Marc Epstein <marc@tendoto.com>
2017-03-24 16:00:18 -0400

    added some comments

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
+#Create a variable that is set to a range of 1 to 100.
 loop_range = (1..100)
 
+#An each loop to go through 1 through 100
 loop_range.each do |number|
    if number % 3 == 0
      puts "fizz"
